159e6a4eb9c78cdbe6b75134a37d4e3507a8f906
Xuan Wang <1751974693@qq.com>
2022-09-03 20:06:53 +0800

    Added feature 4

--- Greeter.java
+++ Greeter.java
@@ -22,7 +22,7 @@ public class Greeter {
 
   public static void main(String[] args) {
     String language = args[0];
-    String country = args[1];
+    String country = args[2];
     String name = args[3];
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
